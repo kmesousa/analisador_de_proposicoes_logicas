84e0420e866b3c208d2f290fdcee21bf02648e50
kmesousa <exoticbutters5xg@gmail.com>
2026-06-03 11:03:23 -0300

    arquivos deletatos e pequenas modificações

--- testes/parenteses.py
+++ testes/parenteses.py
@@ -3,15 +3,9 @@ def parenteseses_certos(texto:str, com=False) -> bool:
     if com!= 0 and com!=False:
         com = True
 
-    # lista = []
-    # for i in range (0, len(texto)):
-    #     lista += texto[i]
-    # print(list(range(1, len(lista))))
     lista = list(texto)
     if com:
         print(lista)
-    #preciso de um passo pra remover os espaços tbm, dps coloco!!!!!!!!!!!!
-    #tbm poderia fazer um pra remover todos os caracteres que não são parenteses (), pra poder reaproveitar essa funçao em outros contextos talvez!!!!!!!!!!!!!!!!!!!!
 
     pilha1 = [] #( não pode conter elementos no final
     pilha2 =[] #) não pode conter elementos caso a pilha 2 esteja vazia a qualquer momento
@@ -23,14 +17,14 @@ def parenteseses_certos(texto:str, com=False) -> bool:
         elif lista[i] == ')':
             pilha2.append(lista[i])
         if com:
-            print(pilha1, pilha2) #essa parte deu certo, blz
+            print(pilha1, pilha2)
 
         #2: pareamento
         while len(pilha2)!=0 and len(pilha1)!=0:
             del pilha1[0]
             del pilha2[0]
             if com:
-                print(pilha1, pilha2) #perfeito
+                print(pilha1, pilha2)
 
         #3: validadação
         if com:
