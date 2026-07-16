f1a4cfde647185682aa147817804d7cb8c5933eb
kmesousa <exoticbutters5xg@gmail.com>
2026-07-16 16:25:04 -0300

    funcoes utilizadas na main para interagir com usuario

--- interface.py
+++ interface.py
@@ -1,25 +1,27 @@
+from dicsOperadores import dic_simbolos
+import funcoes
+
 #configurações
-WIDHT = 50
-sep = lambda: print('-'*WIDHT)
+TAMANHO = 50
+sep = lambda: print('-'*TAMANHO)
 
 # --------------- limpar tela -------------------------------------------
 import os
 
-def clean():
+def limpar_tela():
   if os.name == 'nt':
     _ = os.system('cls')
 
 #---------------- menu principal -----------------------------------------
 def exibir_menu_inicial():
     #--------------- cabeçário ----------------------------------
-    print(' GERADOR E CLASSIFICADOR DE TABELAS VERDADE '.center(WIDHT, '='))
-    print('Lógica Proposicional'.center(WIDHT))
-    print('Use os símbolos abaixo para montar expressões.'.center(WIDHT))
+    print(' GERADOR E CLASSIFICADOR DE TABELAS VERDADE '.center(TAMANHO, '='))
+    print('Lógica Proposicional'.center(TAMANHO))
+    print('Use os símbolos abaixo para montar expressões.'.center(TAMANHO))
     sep()
 
     #------------------ símbolos --------------------------------
-    from dicsOperadores import dic_simbolos
-    print('simbolos'.center(WIDHT))
+    print('simbolos'.center(TAMANHO))
     sep()
     #imprimindo os simbolos dividos em duas colunas, uma ao lado da outra
     chaves = list(dic_simbolos.keys())
@@ -43,19 +45,22 @@ def exibir_menu_inicial():
 def pedir_proposição ():
     print('digite a fórmula proposicional OU digite: ')
     print('[sair] para sair')
+    print('[trocar] para trocar simbolos')
     sep()
     return input(">>>>>> ")
 
-def mostrar_erro ():
-    print('invalido')
+def mostrar_erro (tokens):
+    print(f'invalido: {funcoes.validar(tokens)[1]}')
+    sep()
 
 def perguntar_continuar ():
-    pass
+    print('continuar? (digite [sair] para sair)')
+    return input('>>>>> ')
 
 def exibir_menu_final():
-    sep = lambda: print('='*WIDHT)
+    sep = lambda: print('='*TAMANHO)
     sep()
-    print('FIM DO PROGRAMA'.center(WIDHT))
+    print('FIM DO PROGRAMA'.center(TAMANHO))
     sep()
 
     print('\nGerador de Tabelas Verdade')
