d21d9c04e03969fd832977c7d8af5b47e3baf6d3
kmesousa <exoticbutters5xg@gmail.com>
2026-07-16 15:49:20 -0300

    adicionando dicionario de id e funcao mudar simbolos

--- testes.py
+++
@@ -1,8 +0,0 @@
-from funcoes import *
-
-print(validar(tokenizar("(p ^ q) ^ - q")))
-print(validar(tokenizar("-q")))
-print(validar(tokenizar("- -q")))
-print(validar(tokenizar("- ^ q")))
-print(validar(tokenizar("p ^ (-q)")))
-print(validar(tokenizar("(p ^) q ")))
